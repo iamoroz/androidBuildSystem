4687d14f136c570b730bba0625d68b90d2d23d6d
iamoroz <iamorozpost@gmail.com>
2020-07-16 11:46:58 +0300

    Initial commit

---
+++ app/src/bel/java/com.example.androidbuildsystem/TestKotlinClass.kt
@@ -0,0 +1,7 @@
+package com.example.androidbuildsystem
+
+class TestKotlinClass {
+    fun getSomeString(): String {
+        return "Hello from BEL package"
+    }
+}
---
+++ buildSrc/src/main/java/Dependencies.java
@@ -0,0 +1,3 @@
+public class Dependencies {
+    public static final String androidMaterial = "com.google.android.material:material:1.1.0";
+}
